60d985c5d8f39741f10e7d073c6eb82f3c3dac06
BeefSupreme101 <108609386+BeefSupreme101@users.noreply.github.com>
2022-07-04 12:13:47 +0930

    Added a game thingie

---
+++ src/main/java/GameManager.java
@@ -0,0 +1,37 @@
+package src.main.java;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class GameManager {
+	
+	private GameGraphics gameGraphics;
+	
+	private long cycleTime = 0;
+	private long sleepTime = 0;
+	private boolean running = true;
+
+	private long speed = 70;
+	
+	public GameManager(GameGraphics gameGraphics){
+		this.gameGraphics = gameGraphics;
+	}
+	
+	public void beginGame() {
+
+		while (running) {
+			cycleTime = System.currentTimeMillis();
+			gameGraphics.movePlayer();
+			gameGraphics.renderGame();
+			cycleTime = System.currentTimeMillis() - cycleTime;
+			sleepTime = speed - cycleTime;
+			if (sleepTime < 0)
+				sleepTime = 0;
+			try {
+				Thread.sleep(sleepTime);
+			} catch (InterruptedException ex) {
+				Logger.getLogger(GameManager.class.getName()).log(Level.SEVERE, null,
+						ex);
+			}
+		}
+	}
+}
